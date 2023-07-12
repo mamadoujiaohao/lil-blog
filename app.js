b86c34b6a8cd07b2e89cd2a1112138c421293d39
aking4511 <aking4511@gmail.com>
2023-07-12 16:05:03 +0800

    feat: add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const port = process.env.PORT || 3000
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.urlencoded({ extened: true }))
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+module.exports = app
