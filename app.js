dc740d1466f48ff45dc2df591c14d262287a0189
Arthur <aking4511@gmail.com>
2023-07-30 15:26:51 +0800

    feat: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+
 const port = process.env.PORT || 3000
 const routes = require('./routes')
+const { engine } = require('express-handlebars')
 
 const app = express()
 
+app.engine('hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 app.use(express.urlencoded({ extened: true }))
 app.use(express.json())
 app.use(routes)
